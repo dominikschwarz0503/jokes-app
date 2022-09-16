4579f6342744a3059035c23bdf44d986743e9f68
dominikschwarz0503 <dominik.schwarz@otto.de>
2022-09-16 11:30:56 +0200

    Add copy to clipboard function

--- src/components/JokeContainer/JokeContainer.tsx
+++ src/components/JokeContainer/JokeContainer.tsx
@@ -70,6 +70,15 @@ export default function JokeContainer(props: any) {
         }
     };
 
+    const copyJoke = () => {
+        const joke = document.querySelector(".joke");
+
+        if (navigator.clipboard) {
+            navigator.clipboard.writeText(joke?.textContent as string);
+            console.log("copied to clipboard!");
+        }
+    };
+
     return (
         <>
             <div className="container">
@@ -79,13 +88,14 @@ export default function JokeContainer(props: any) {
                     viewBox="0 0 24 24"
                     fill="#fffff"
                     xmlns="http://www.w3.org/2000/svg"
+                    onClick={() => copyJoke()}
                 >
                     <path
                         d="M8 5H6C4.89543 5 4 5.89543 4 7V19C4 20.1046 4.89543 21 6 21H16C17.1046 21 18 20.1046 18 19V18M8 5C8 6.10457 8.89543 7 10 7H12C13.1046 7 14 6.10457 14 5M8 5C8 3.89543 8.89543 3 10 3H12C13.1046 3 14 3.89543 14 5M14 5H16C17.1046 5 18 5.89543 18 7V10M20 14H10M10 14L13 11M10 14L13 17"
                         stroke="#4A5568"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                     />
                 </svg>
             </div>
