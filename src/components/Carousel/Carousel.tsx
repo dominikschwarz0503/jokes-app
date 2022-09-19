d0c3d920092df5f928b5afb4530f22672e782378
dominikschwarz0503 <dominik.schwarz@otto.de>
2022-09-19 14:58:33 +0200

    Add image carousel

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,15 +1,38 @@
-import { Key } from "react";
+import axios from "axios";
+import "./Carousel.css";
+import { useEffect, useState } from "react";
 
 export default function Carousel(props: any) {
-    console.log(props.content);
+    const [isLoading, setIsLoading] = useState(true);
+    const [data, setData] = useState<any[]>([]);
+
+    useEffect(() => {
+        const url = `https://meme-api.herokuapp.com/gimme/${props.currentSubreddit}/5`;
+        axios
+            .get(url)
+            .then((response) => setData(response.data.memes))
+            .catch((error) => console.log(error));
+    }, []);
+
+    useEffect(() => {
+        if (data.length !== 0) {
+            setIsLoading(false);
+        }
+
+        console.log(data);
+    }, [data]);
 
     return (
         <>
             <div className="carousel-container">
                 <div className="carousel-content">
-                    {props.content.map((content: string, key: Key) => (
-                        <img src={content} alt="Meme" key={key} />
-                    ))}
+                    {isLoading ? (
+                        <p>Loading...</p>
+                    ) : (
+                        data.map((meme: any, key) => (
+                            <img src={meme.url} alt="Meme" key={key} />
+                        ))
+                    )}
                 </div>
             </div>
         </>
--- src/components/MemeContainer/MemeContainer.tsx
+++ src/components/MemeContainer/MemeContainer.tsx
@@ -4,14 +4,13 @@ import Carousel from "../Carousel/Carousel";
 import "./MemeContainer.css";
 
 export default function MemeContainer(this: any, props: any) {
-    const [currentSubreddit, setCurrentSubreddit] = useState("dankmemes");
-    const images: any = ["https://i.redd.it/kfyoyidhs7o91.gif"];
+    const images: any = [];
 
     const URLs = {
-        memeapi: `https://meme-api.herokuapp.com/gimme/${currentSubreddit}`,
+        memeapi: `https://meme-api.herokuapp.com/gimme/${props.currentSubreddit}/5`,
     };
 
-    const [isConnected, setIsConnected] = useState(false);
+    const [hasLoaded, setHasLoaded] = useState(false);
 
     useEffect(() => {
         requestMeme();
@@ -20,47 +19,29 @@ export default function MemeContainer(this: any, props: any) {
     }, []);
 
     const requestMeme = async () => {
-        const container = document.querySelector(".meme-container");
-
         const headers = {
             Accept: "application/json",
         };
 
         try {
             await axios.get(URLs.memeapi, { headers }).then((response) => {
-                setIsConnected(true);
+                const memes = response.data.memes;
 
-                const meme = response.data.url;
+                memes.forEach((meme: any) => {
+                    images.push(meme);
+                });
 
-                images.push(meme);
-                console.log(images);
+                setHasLoaded(true);
             });
         } catch (error) {
             console.log("ERROR", error);
         }
     };
 
-    const getSelectedSubreddit = (event: any) => {
-        setCurrentSubreddit(event.target.value);
-    };
-
     return (
         <>
-            <select
-                name="memes"
-                id="subreddit-menu"
-                onChange={getSelectedSubreddit}
-            >
-                <option value="Choose a Subreddit" disabled defaultChecked>
-                    Choose a subreddit
-                </option>
-                <option value="ich_iel">ich_iel</option>
-                <option value="dankmemes">dankmemes</option>
-                <option value="memes">Memes</option>
-                <option value="programmerhumor">Programmer Humor</option>
-            </select>
+            {hasLoaded ? <Carousel content={images} /> : ""}
 
-            <Carousel content={images} />
             <div className="meme-container"></div>
         </>
     );
